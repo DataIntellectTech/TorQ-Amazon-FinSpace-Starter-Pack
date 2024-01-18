59dbd96a6558bcbcabbf6659eb54d85d344d2939
Eugene Temlock <eugene.temlock@dataintellect.com>
2024-01-18 15:36:54 +0000

    merge local changes with master. Perform integration tests when ready

--- terraform-deployment/lambda/src/create_cluster_onConflict.py
+++ terraform-deployment/lambda/src/create_cluster_onConflict.py
@@ -68,7 +68,7 @@ def lambda_handler(event, context):
         'environmentId': envId,
         'clusterName': newClusterId,
         'clusterType': event["clusterType"],
-        'clusterDescription': "new rdb cluster",
+        'clusterDescription': f"cluster for process {newClusterId}",
         'capacityConfiguration': clusterInfo['capacityConfiguration'],
         'releaseLabel': clusterInfo['releaseLabel'],
         'vpcConfiguration': clusterInfo['vpcConfiguration'],
