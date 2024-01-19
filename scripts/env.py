3b48d47c27908fa6277ec9bba1231aec39f54fee
Eugene Temlock <eugene.temlock@dataintellect.com>
2024-01-19 16:33:38 +0000

    updating scripts/create_cluster.py

--- scripts/env.py
+++ scripts/env.py
@@ -1,10 +1,10 @@
 # config
-environmentId = "rtci3jl7pii6tyhp5d2shk"
-clusterDescription = "fintorq testing"
+environmentId = "33c4rqbmsx7bjclrrwb2gc"
+clusterDescription = ""
 
 # database settings
-databaseName = "finTorq_hdb"
-changesetId = "RMSiPOI0nu8U7aO1EjkOEA"
+databaseName = "finspace-database"
+changesetId = "XsYLTlRykQ9qvLTTqdjLGQ"
 
 # node settings
 nodeType = "kx.s.large"
@@ -12,30 +12,31 @@ nodeCount = 1
 
 # availability mode - "SINGLE" or "MULTI"
 azMode = "SINGLE" 
-availabilityZoneId = "euw1-az1"
+availabilityZoneId = "use1-az6"
 
 # code settings
-s3Bucket = "fintorq-eu-west1-code"
-s3Key = "code.zip"
+s3Bucket = "finspace-code-bucket-virginia"
+s3Key = "35551e91bbeba4da234259e17734555e65b09e88.zip"
 initScript = "TorQ-Amazon-FinSpace-Starter-Pack/env.q"
 
-executionRole = "arn:aws:iam::766012286003:role/finTorq_hdb_connect"
+executionRole = "arn:aws:iam::766012286003:role/finspace-role-virginia"
 
 commandLineArguments = [
-    {"key":"noredirect", "value":"true"}
+    {"key":"noredirect", "value":"true"},
+    {"key":"jsonlogs", "value":"true"}
     ]
 
 savedownStorageConfiguration={
         'type': 'SDS01',
-        'size': 50
+        'size': 10
     }
 
 # network settings
-vpcId = "vpc-079ebe5fbb9198243"
-subnetIds = ["subnet-0a547926c2f5f6c8d"] # can be a list
-securityGroupIds = ["sg-06148e5f102f9f1dc", "sg-042c39003af04b1ce"] # can be a list
+vpcId = "vpc-0a5b1db302525d776"
+subnetIds = ["subnet-00dbf269c0172ec77"] # can be a list
+securityGroupIds = ["sg-09d7de163f7d0327b", "sg-029197cc9263e4f00"] # can be a list
 ipAddressType = "IP_V4"
 
 releaseLabel = "1.0"
 
-region="eu-west-1"
+region="us-east-1"
