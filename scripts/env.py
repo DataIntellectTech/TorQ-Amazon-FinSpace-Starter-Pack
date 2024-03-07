0e75041a9a0edf72c83a455a8a1aac1f7a24c4e5
Eugene Temlock <eugene.temlock@dataintellect.com>
2024-03-07 20:02:19 +0000

    current configs

--- scripts/env.py
+++ scripts/env.py
@@ -16,7 +16,8 @@ availabilityZoneId = "use1-az6"
 
 # code settings
 s3Bucket = "finspace-code-bucket-virginia"
-s3Key =  "945acf0f36eafdc885757fa1c622d094efc405a7.zip"
+#s3Key =  "945acf0f36eafdc885757fa1c622d094efc405a7.zip"
+s3Key = "code_rel.zip"
 initScript = "TorQ-Amazon-FinSpace-Starter-Pack/env.q"
 
 executionRole = "arn:aws:iam::766012286003:role/finspace-role-virginia"
@@ -32,7 +33,7 @@ savedownStorageConfiguration={
     }
 
 # flag for HDB to use cache or not
-useCache=True
+useCache=False
 
 cacheConfiguration=[{
     'cacheType':'CACHE_1000',
